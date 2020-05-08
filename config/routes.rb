ceee785bcac19dd9bcec30c7aaf899a979815839
nishio <nishio.ryota.1997@gmail.com>
2020-05-08 20:39:15 +0900

    homeのページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # local...3000にアクセスしたときにcocktailsに誘導する
-  root to: 'cocktails#index'
+  root to:  'home#index'
   # 全てのアクションを一括管理
   resources :cocktails
+  # getリクエスト定義
+  get 'home/index'
 end
