3b017b060b200ee49103ccf244e1550de1d96835
nishio <nishio.ryota.1997@gmail.com>
2020-05-10 04:53:22 +0900

    モデル検証を作成

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,7 @@
 class Cocktail < ApplicationRecord
     validates :name, presence: true
+    validates :base_alcohol, presence: true
+    validates :taste, presence: true
+    validates :alcohol_percentage, presence: true
+    validates :glass_type, presence: true
 end
