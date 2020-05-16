faf3081a08df05dab46338501e30044424ddf257
nishio <nishio.ryota.1997@gmail.com>
2020-05-17 04:12:25 +0900

    タイトルがあるかテスト

--- spec/system/cocktail_spec.rb
+++ spec/system/cocktail_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "SiteLayouts", type: :system do
+RSpec.describe "ホーム画面のテスト", type: :system do
   # テスト前に実行
   before do
     # テストカクテル作成
     FactoryBot.create(:cocktail)
   end
 
-    describe "home layout" do    
+    describe "ホーム画面のレイアウト" do    
+      it "ホームタイトルがカクテルホーム" do
+        visit home_path
+        # have_selecter でタグを指名
+        expect(page).to have_selector 'h1', text: 'カクテルホーム'
+      end
       it "ページレイアウト" do
         visit cocktails_path
+        expect(page).to have_selector 'h1', text: 'カクテル検索結果'
         expect(page).to have_content 'テストネーム'
       end
     end
