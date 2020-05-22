81b6d2cd4636a87addd19e6d599a32f58f82d353
nishio <nishio.ryota.1997@gmail.com>
2020-05-22 19:53:36 +0900

    検索機能の追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @cocktails = Cocktail.all
+    @q = Cocktail.ransack(params[:q])
+    @cocktails = @q.result(distinct: true)
   end
 end
