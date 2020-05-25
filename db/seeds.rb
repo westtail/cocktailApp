3db556f83e4a9b2f88dd1b9e831c9796ffdd1a08
nishio <nishio.ryota.1997@gmail.com>
2020-05-26 01:26:06 +0900

    seedデータ作成、検索部分の追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create!(name: "ウイスキー", base_alcohol: "ウイスキー", taste: "辛い", alcohol_percentage: 40, glass_type:"ロング",cocktail_recipe: "ウイスキー原液")
+Cocktail.create!(name: "ラム", base_alcohol: "ラム", taste: "辛い", alcohol_percentage: 40, glass_type:"ロング",cocktail_recipe: "ラム原液")
+Cocktail.create!(name: "ウォッカ ", base_alcohol: "ウォッカ", taste: "辛い", alcohol_percentage: 40, glass_type:"ロング",cocktail_recipe: "ウォッカ原液")
+Cocktail.create!(name: "ジン", base_alcohol: "ジン", taste: "辛い", alcohol_percentage: 40, glass_type:"ロング",cocktail_recipe: "ジン原液")
+5.times do |n|
+    Cocktail.create!(
+      name: "ウイスキーロック",
+      base_alcohol: "ウイスキー",
+      taste: "辛い",
+      alcohol_percentage: n * 10,
+      glass_type:"ロング",
+      cocktail_recipe: "ウイスキー原液"
+    )
+  end
