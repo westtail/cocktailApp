24fdd688bc759ee9e47a26e287455d79ac3ef0e9
nishio <nishio.ryota.1997@gmail.com>
2020-06-01 22:44:47 +0900

    nginxの設定の微調整

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,7 +1,11 @@
 #bundle exec rake db:migrate
+#データベース作成
 rails db:create
+#データベース作成 マイグレード
 rails db:migrate
+#データベース作成 データ追加
 rails db:seed
+# その他のコマンド
 cp -rf /tmp/public/* /cocktailApp/public/
 mkdir -p tmp/sockets
 bundle exec puma -C config/puma.rb
